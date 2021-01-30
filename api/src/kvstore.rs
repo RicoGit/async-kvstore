3197247ef5d51575eaf37440cb32c7df5ac5607f
C.Solovev <constantine.solovev@gmail.com>
2021-01-30 20:31:34 +0400

    Add Error type

--- api/src/kvstore.rs
+++ api/src/kvstore.rs
@@ -2,20 +2,27 @@
 
 use std::future::Future;
 use std::pin::Pin;
-
-pub type Task<'a, Val> = Pin<Box<dyn Future<Output = Val> + Send + 'a>>;
+use thiserror::Error;
 
 /// Aggregated trait for KVStore
 pub trait KVStore<K: Send, V: Send>: GetOp<K, V> + SetOp<K, V> {}
 
+#[derive(Error, Debug)]
+#[error("KVStore Error: {msg:?}")]
+pub struct KVStoreError {
+    msg: String,
+}
+
+pub type StoreTask<'a, Val> = Pin<Box<dyn Future<Output = Result<Val, KVStoreError>> + Send + 'a>>;
+
 /// Get value by key
 pub trait GetOp<K: Send, V: Send> {
-    fn get(&self, key: K) -> Task<Option<V>>;
+    fn get(&self, key: K) -> StoreTask<Option<V>>;
 }
 
 /// Set value by key
 pub trait SetOp<K: Send, V: Send> {
-    fn set(&mut self, key: K, val: V) -> Task<Option<V>>;
+    fn set(&mut self, key: K, val: V) -> StoreTask<Option<V>>;
 }
 
 // todo add remove, travers (is possible)
--- inmemory/src/hashmap_store.rs
+++ inmemory/src/hashmap_store.rs
@@ -11,7 +11,7 @@
 //!     use kvstore_inmemory::hashmap_store::HashMapKVStore;
 //!     let mut store = HashMapKVStore::new();
 //!     store.set(1, "test").await;
-//!     let res = store.get(1).await;
+//!     let res = store.get(1).await.unwrap();
 //!     assert_eq!(res, Some("test"))
 //! }
 //!
@@ -46,7 +46,7 @@ where
     K: Hash + Eq + Send,
     V: Send + Clone,
 {
-    fn get(&self, key: K) -> Task<Option<V>> {
+    fn get(&self, key: K) -> StoreTask<Option<V>> {
         let res = self
             .data
             .clone()
@@ -54,7 +54,7 @@ where
             .map(|lock| lock.get(&key).cloned())
             .ok()
             .flatten();
-        Box::pin(async move { res })
+        Box::pin(async move { Ok(res) })
     }
 }
 
@@ -63,7 +63,7 @@ where
     K: Hash + Eq + Send,
     V: Send + Clone,
 {
-    fn set(&mut self, key: K, val: V) -> Task<Option<V>> {
+    fn set(&mut self, key: K, val: V) -> StoreTask<Option<V>> {
         let res = self
             .data
             .clone()
@@ -71,7 +71,7 @@ where
             .map(|mut lock| lock.insert(key, val))
             .ok()
             .flatten();
-        Box::pin(async move { res })
+        Box::pin(async move { Ok(res) })
     }
 }
 
@@ -83,16 +83,16 @@ mod tests {
     async fn set_and_get() {
         let mut storage = HashMapKVStore::new();
 
-        assert_eq!(storage.get("test").await, None);
-        assert_eq!(storage.set("test", 32).await, None);
-        assert_eq!(storage.get("test").await, Some(32));
+        assert_eq!(storage.get("test").await.unwrap(), None);
+        assert_eq!(storage.set("test", 32).await.unwrap(), None);
+        assert_eq!(storage.get("test").await.unwrap(), Some(32));
 
-        assert_eq!(dbg!(storage.set("test", 42).await), Some(32));
-        assert_eq!(storage.get("test").await, Some(42));
+        assert_eq!(dbg!(storage.set("test", 42).await.unwrap()), Some(32));
+        assert_eq!(storage.get("test").await.unwrap(), Some(42));
 
-        assert_eq!(storage.get("test2").await, None);
-        assert_eq!(storage.set("test2", 2).await, None);
-        assert_eq!(storage.set("test3", 3).await, None);
-        assert_eq!(storage.get("test2").await, Some(2));
+        assert_eq!(storage.get("test2").await.unwrap(), None);
+        assert_eq!(storage.set("test2", 2).await.unwrap(), None);
+        assert_eq!(storage.set("test3", 3).await.unwrap(), None);
+        assert_eq!(storage.get("test2").await.unwrap(), Some(2));
     }
 }
